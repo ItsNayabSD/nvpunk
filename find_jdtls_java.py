9f8c2213716d2f45e4aaef073cf1388017c8b6a3
Gabriele Musco <gabmus@disroot.org>
2023-01-17 22:28:17 +0100

    improve find_jdtls_java script

--- find_jdtls_java.py
+++ find_jdtls_java.py
@@ -3,21 +3,26 @@ from pathlib import Path
 
 
 def get_version(java_exec: str) -> int:
-    out, _ = Popen(
-        [java_exec, '-version'], stdout=PIPE, stderr=STDOUT
-    ).communicate()
-    lines = [
-        line for line in out.decode().split('\n')
-        if 'version' in line and '"' in line and '.' in line
-    ]
-    if not any(lines):
+    try:
+        out, _ = Popen(
+            [java_exec, '-version'], stdout=PIPE, stderr=STDOUT
+        ).communicate()
+        lines = [
+            line for line in out.decode().split('\n')
+            if 'version' in line and '"' in line and '.' in line
+        ]
+        if not any(lines):
+            return 0
+        return int(lines[0].split('"')[1].split('.')[0])
+    except Exception:
         return 0
-    return int(lines[0].split('"')[1].split('.')[0])
 
 
 jvm_dir = Path('/usr/lib/jvm/')
 
 for dir in jvm_dir.iterdir():
+    if not dir.is_dir():
+        continue
     if get_version(str(dir.joinpath('bin/java'))) == 17:
         print(dir)
         exit(0)
