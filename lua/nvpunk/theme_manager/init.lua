7d07f34fa8f11c24f5f25c605bd7126a53d03c4c
Gabriele Musco <gabmus@disroot.org>
2023-01-26 15:02:32 +0100

    fix theme loading on new installs

--- lua/nvpunk/theme_manager/init.lua
+++ lua/nvpunk/theme_manager/init.lua
@@ -3,8 +3,7 @@ local M = {}
 local theme = require('nvpunk.preferences').get_theme()
 
 M.setup = function()
-    require('nvpunk.internals.try').load_theme(
-        theme,
+    vim.schedule(
         function()
             require('nvpunk.theme_manager.theme_chooser').load_theme(
                 theme,
