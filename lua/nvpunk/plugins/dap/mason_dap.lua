45bd5021fb47b98ec777134b791042417b46609e
Gabriele Musco <gabmus@disroot.org>
2023-03-05 18:21:04 +0100

    fix typescript debugging

--- lua/nvpunk/plugins/dap/mason_dap.lua
+++ lua/nvpunk/plugins/dap/mason_dap.lua
@@ -10,7 +10,21 @@ return {
                 'python',
             },
             automatic_installation = true,
-            automatic_setup = true,
+            automatic_setup = {
+                configurations = function(default)
+                    local outFiles = {
+                        '${workspaceFolder}/dist/**/*.js',
+                        '${workspaceFolder}/build/**/*.js',
+                        '${workspaceFolder}/lib/**/*.js',
+                        '${workspaceFolder}/public/**/*.js',
+                        '${workspaceFolder}/out/**/*.js',
+                    }
+                    default.node2[1].outFiles = outFiles
+                    default.node2[2].outFiles = outFiles
+
+                    return default
+                end
+            }
         }
         mason_dap.setup_handlers()
     end,
