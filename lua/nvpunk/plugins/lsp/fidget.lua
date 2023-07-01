68d4e5fdbf158129e631ed71b037b27f117b37f9
Gabriele Musco <gabmus@disroot.org>
2023-07-01 13:21:50 +0200

    updated plugins

--- lua/nvpunk/plugins/lsp/fidget.lua
+++ lua/nvpunk/plugins/lsp/fidget.lua
@@ -1,6 +1,7 @@
 -- show language server starting progress in the lower right corner
 return {
     'j-hui/fidget.nvim',
+    branch = 'legacy',
     config = function()
         require('fidget').setup {
             text = {
